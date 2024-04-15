025cb9a455dcc7d2a2f6af356bc05d4f0d7e6d45
Gleb <glebmaskalev712@gmail.com>
2024-04-15 20:27:48 +0500

    Изменение анимации

---
+++ Scripts/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var speed = 120
+
+# const
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# @onready
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+
+func get_input():
+	var input_direction = Input.get_vector("move left", "move right", "move up", "move down")
+	velocity = input_direction * speed
