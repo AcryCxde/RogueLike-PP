7958916e18835e9ea50c23e8f5969badaa213308
Gleb <glebmaskalev712@gmail.com>
2024-05-12 23:18:19 +0500

    Код персонажа

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,20 +1,23 @@
+# иконка персонажа в меню сцены
+@icon("res://Assets/Character/Character.png")
+
 extends CharacterBody2D
 
-var speed = 250
+const SPEED = 350
 
-# const
-
+# гравитация, можно изменить в настройках проекта: Physics/2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# @onready
-
-func _ready():
-	pass
-
 func _physics_process(delta):
-	get_input()
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	velocity = velocity.normalized() * SPEED
 	move_and_slide()
-
-func get_input():
-	var input_direction = Input.get_vector("move left", "move right", "move up", "move down")
-	velocity = input_direction * speed
