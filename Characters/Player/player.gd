8929a62f5bfdadd818649d5735039d59ac2e8376
Gleb <glebmaskalev712@gmail.com>
2024-05-18 22:21:29 +0500

    Добавление оружия

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -1,14 +1,18 @@
 extends Character
 
+@onready var weapon: Node2D = get_node("Weapon/WeaponSprite")
 
 func _process(_delta) -> void:
 	var mouse_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 	
 	if mouse_direction.x > 0 and animated_sprite.flip_h:
 		animated_sprite.flip_h = false
+		weapon.flip_v = false
 	elif mouse_direction.x < 0 and not animated_sprite.flip_h:
 		animated_sprite.flip_h = true
-
+		weapon.flip_v = true
+	
+	weapon.rotation = mouse_direction.angle()
 
 func get_input() -> void:
 	move_direction = Vector2.ZERO
