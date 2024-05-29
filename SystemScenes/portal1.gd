03a03d6a37a362e73c48c955fadca5ce876256ef
AcryCxde <130891938+AcryCxde@users.noreply.github.com>
2024-05-29 22:20:36 +0500

    ///

--- SystemScenes/portal1.gd
+++ SystemScenes/portal1.gd
@@ -8,8 +8,7 @@ var position2 = Vector2(1000.0, -22000.0)
 
 func _on_player_entered(_body: PhysicsBody2D):
 	entered = true
-	if entered == true:
-		var _nextLevel = allLevels.pick_random()
+
 
 func _on_player_exited(_body: PhysicsBody2D):
 	entered = false
--- SystemScenes/portal2.gd
+++ SystemScenes/portal2.gd
@@ -1,13 +1,11 @@
 extends Area2D
 
-var allMaps = [preload("res://Levels/lvl_1.tscn"), preload("res://Levels/lvl_2.tscn")]
+
 var entered = false
 
 func _on_player_entered(_body: PhysicsBody2D):
 	entered = true
-	if entered == true:
-		var nextScene = allMaps.pick_random()
-		get_tree().change_scene_to_packed(nextScene)
+
 
 
 func _on_player_exited(_body: PhysicsBody2D):
