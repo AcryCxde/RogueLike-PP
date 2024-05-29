ecdd6248210daf0e3648a30074fbb153910c9083
streaxyakasieh <131168538+streaxyakasieh@users.noreply.github.com>
2024-05-29 22:20:02 +0500

    Телепорты готовы
    Телепорты для начала игры, выхода в лобби и из игры

--- SystemScenes/portal1.gd
+++ SystemScenes/portal1.gd
@@ -1,15 +1,37 @@
 extends Area2D
 
-@onready var player = get_tree().get_first_node_in_group("player")
-var allLevels = [position1, position2]
+var level1 = Vector2(15670, 798)
+var level2 = Vector2(1000, -21500)
+var levelpick = [1,2,3,4,5,6]
+#1 и 3 - первый уровень
+#2 и 5 - второй уровень
+#4 и 6 - третий уровень
 var entered = false
-var position1 = Vector2(15670.0, 798.0)
-var position2 = Vector2(1000.0, -22000.0)
+var nextLevel
 
 func _on_player_entered(_body: PhysicsBody2D):
 	entered = true
-	if entered == true:
-		var _nextLevel = allLevels.pick_random()
+	nextLevel = levelpick.pick_random()
+	
+	if (nextLevel == 1) or (nextLevel == 3):
+		if entered == true:
+			if "Player" in _body.name:
+				entered = false
+				_body.global_position = level1
+	
+	if (nextLevel == 2) or (nextLevel == 5):
+		if entered == true:
+			if "Player" in _body.name:
+				entered = false
+				_body.global_position = level2
+	
+	#if (nextLevel == 4) or (nextLevel == 6):
+		#if entered == true:
+			#if "Player" in _body.name:
+				#entered = false
+				#_body.global_position = level3
+
+
 
 func _on_player_exited(_body: PhysicsBody2D):
 	entered = false
